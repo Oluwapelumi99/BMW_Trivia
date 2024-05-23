a3ebecac7aad038b3a0c71df97aa0ec327fd8c9d
Oluwapelumi Fadare <oluwajosephine7@gmail.com>
2024-05-23 22:30:01 +0000

    Add trivia questions 6-11 and validate that users answers are working

--- run.py
+++ run.py
@@ -14,14 +14,23 @@ else:
     print('[bold white] Wrong input. Please try again..')
 
 
-def get_user_answer_1(user_answer):
+def get_user_answer_0(user_answer):
     if answer.lower() == 'a':
         print(f"[bold blue]Welldone! {user_answer} is the correct answer")
     else:
         print('[italic bold white]"Sorry option a is the correct answer"')    
  
-answer= input('[bold blue]1. What does BMW stand for? \n A.Bayerische Motoren Werke \n B. \n Pick your answer: ')
-get_user_answer_1(answer)
+answer= input('[bold blue]1. What does BMW stand for? \n A. Bayerische Motoren Werke \n B. Brunswick Motoren Werke \n C. Borgholzzhausen Motoren Werke\n D. Berlin Motorenn Werke\n Pick your answer: ')
+get_user_answer_0(answer)
+
+def get_user_answer_0(user_answer):
+    if answer.lower() == 'd':
+        print(f"[bold blue]Welldone! {user_answer} is the correct answer")
+    else:
+        print('[italic bold white]"Sorry option d is the correct answer"')    
+ 
+answer= input('[bold blue]2. What color is the BMW logo \n A. Blue & gold \n B. Red & gold \n C. Red & White \n D. Blue & white \n Pick your answer: ')
+get_user_answer_0(answer)
 
 
 def get_user_answer_2(user_answer):
@@ -29,7 +38,8 @@ def get_user_answer_2(user_answer):
         print(f"[bold blue]Welldone! {user_answer} is the correct answer")
     else:
         print('[bold white]"Sorry option b is the correct answer"')
-answer = input('[bold white]2. What is the slogan of BMW? \n A. The Ultimate Driving Power \n B. Sheer Driving Pleasure \n C. Enjoy the Ride \n D. Joy of discovery \n Pick your answer: ')
+answer = input('[bold blue]3. What is the slogan of BMW? \n A. The Ultimate Driving Power \n B. Sheer Driving Pleasure\n'
+                  'C. Enjoy the Ride \n D. Joy of discovery \n Pick your answer: ')
 get_user_answer_2(answer)    
    
 def get_user_answer_3(user_answer):
@@ -37,7 +47,8 @@ def get_user_answer_3(user_answer):
         print(f"[bold blue]Welldone! {user_answer} is the correct answer")
     else:
         print('[bold white]"Sorry option c is the correct answer"')
-answer = input('[bold white]2. What is the BMW M series? \n A. A line of electric cars \n B. An SUV for off-roading \n C. A line of performance cars \n D. A line of luxury sedan \n Pick your answer: ')
+answer = input('[bold white]4. What is the BMW M series? \n A. A line of electric cars \n B. An SUV for off-roading \n' 
+                'C. A line of performance cars \n D. A line of luxury sedan \n Pick your answer: ')
 get_user_answer_3(answer)  
 
 
@@ -46,7 +57,8 @@ def get_user_answer_4(user_answer):
         print(f"[bold blue]Welldone! {user_answer} is the correct answer")
     else:
         print('[blue]"Sorry option c is the correct answer"')
-answer = input('[bold white]2. What was the name of BMW\'s first car? \n A. 3 Series \n B. 5 Series \n C. 7 Series\n D. Dixi \n Pick your answer: ')
+answer = input('[bold white]5. What was the name of BMW\'s first car? \n A. 3 Series \n B. 5 Series \n' 
+                'C. 7 Series\n D. Dixi \n Pick your answer: ')
 get_user_answer_4(answer)  
 
 def get_user_answer_5(user_answer):
@@ -54,5 +66,60 @@ def get_user_answer_5(user_answer):
         print(f"[bold blue]Welldone! {user_answer} is the correct answer")
     else:
         print('[blue]"Sorry option c is the correct answer"')
-answer = input('[bold white]2. What year was the BMW 3 Series first intoduced? \n A. 1969 \n B. 1961 \n C. 1975\n D. 1984 \n Pick your answer: ')
+answer = input('[bold white]6. What year was the BMW 3 Series first intoduced? \n A. 1969 \n B. 1961 \n' 
+                'C. 1975\n D. 1984 \n Pick your answer: ')
 get_user_answer_5(answer)  
+
+def get_user_answer_6(user_answer):
+    if answer.lower() == 'c':
+        print(f"[bold blue]Welldone! {user_answer} is the correct answer")
+    else:
+        print('[blue]"Sorry option c is the correct answer"')
+answer = input('[bold white]7. How far could BMW\'s first all-electric car drive before running out of battery? \n A. 79 miles\n B. 59 miles \n C. 19 miles\n D. 39 miles\n Pick your answer: ')
+get_user_answer_6(answer) 
+
+def get_user_answer_7(user_answer):
+    if answer.lower() == 'b':
+        print(f"[bold blue]Welldone! {user_answer} is the correct answer")
+    else:
+        print('[blue]"Sorry option b is the correct answer"')
+answer = input('[bold white]8. What is BMW\'s Formula One racing team called? \n A. BMW Motorsport \n B. BMW Sauber F1 Team \n C. BMW M Racing\n D. BMW Racing Team \n Pick your answer: ')
+get_user_answer_7(answer)
+
+def get_user_answer_8(user_answer):
+    if answer.lower() == 'd':
+        print(f"[bold blue]Welldone! {user_answer} is the correct answer")
+    else:
+        print('[blue]"Sorry option d is the correct answer"')
+answer = input('[bold white]9. What is BMW\'s headquartwes shaped like? \n A. A Radiator \n B. A catalytic converter \n C. A Piston\n D. A Four-Cylinder Engine \n Pick your answer: ')
+get_user_answer_8(answer)
+
+def get_user_answer_9(user_answer):
+    if answer.lower() == 'a':
+        print(f"[bold blue]Welldone! {user_answer} is the correct answer")
+    else:
+        print('[blue]"Sorry option a is the correct answer"')
+answer = input('[bold white]10. What was BMW\'s first product? \n A. Aircraft Engine \n B. Spark plug \n C. A Tractor\n D. Motorcycle \n Pick your answer: ')
+get_user_answer_9(answer)
+
+def get_user_answer_10(user_answer):
+    if answer.lower() == 'b':
+        print(f"[bold blue]Welldone! {user_answer} is the correct answer")
+    else:
+        print('[blue]"Sorry option b is the correct answer"')
+answer = input('[bold white]11. What grille design has become a defining feature of BMW\'s automobiles? \n A. Heart grille \n B. Kidney grille \n C. Pharynx grille\n D. Liver grille \n Pick your answer: ')
+get_user_answer_10(answer)
+
+
+def get_user_answer_11(user_answer):
+    if answer.lower() == 'b':
+        print(f"[bold blue]Welldone! {user_answer} is the correct answer")
+    else:
+        print('[blue]"Sorry option b is the correct answer"')
+answer = input('[bold white]12. What grille design has become a defining feature of BMW\'s automobiles? \n A. Heart grille \n B. Kidney grille \n C. Pharynx grille\n D. Liver grille \n Pick your answer: ')
+get_user_answer_11(answer)
+
+
+
+ 
+
