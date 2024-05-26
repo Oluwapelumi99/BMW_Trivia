873840250b029bcea20072a3665116e3380e4563
Oluwapelumi Fadare <oluwajosephine7@gmail.com>
2024-05-26 13:36:57 +0000

    Add questions in dictionaries form and validate answers are correct

--- run.py
+++ run.py
@@ -15,8 +15,13 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Bmw_trivia')
-
-print('[bold blue]WELCOME TO MY BMW TRIVIA!!! :dance:') 
+def get_username(name):
+    return input(name)
+def print_name(value_name):
+    print(value_name)
+name = get_username('Enter your name:')
+print_name(f'[bold blue]Hello {name}. WELCOME TO MY BMW TRIVIA!!! :smile:')
+  
 
 # import ascii_magic
 # output = ascii_magic.from_images_file('IMG_9484.JPG',columns=200,char='#') 
@@ -34,208 +39,42 @@ while start_trivia:
             start_trivia == False                     
     except Exception:
         print('[bold white] Wrong input. Please try again..')       
-def get_user_answer_0(user_answer):
-    if answer.lower() == 'a':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[italic bold blue]Sorry...option a is the correct answer :frown:')    
- 
-answer= input('[bold blue]1. What does BMW stand for? \n A. Bayerische Motoren Werke \n B. Brunswick Motoren Werke \n C. Borgholzzhausen Motoren Werke\n D. Berlin Motorenn Werke\n Pick your answer: \n ')
-get_user_answer_0(answer)
-
-def get_user_answer_0(user_answer):
-    if answer.lower() == 'd':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[italic bold blue]"Sorry...option d is the correct answer"')    
- 
-answer= input('[bold blue]2. What color is the BMW logo \n A. Blue & gold \n B. Red & gold \n C. Red & White \n D. Blue & white \n Pick your answer: \n ')
-get_user_answer_0(answer)
-
-def get_user_answer_2(user_answer):
-    if answer.lower() == 'b':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[bold blue]"Sorry...option b is the correct answer"')
-answer = input('[bold blue]3. What is the slogan of BMW? \n A. The Ultimate Driving Power \n B. Sheer Driving Pleasure\n C. Enjoy the Ride \n D. Joy of discovery \n Pick your answer: \n ')
-get_user_answer_2(answer)    
-   
-def get_user_answer_3(user_answer):
-    if answer.lower() == 'c':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[bold blue]"Sorry...option c is the correct answer"')
-answer = input('[bold white]4. What is the BMW M series? \n A. A line of electric cars \n B. An SUV for off-roading \n C. A line of performance cars \n D. A line of luxury sedan \n Pick your answer: \n ')
-get_user_answer_3(answer)  
 
 
-def get_user_answer_4(user_answer):
-    if answer.lower() == 'd':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option c is the correct answer"')
-answer = input('[bold blue]5. What was the name of BMW\'s first car? \n A. 3 Series \n B. 5 Series \n C. 7 Series\n D. Dixi \n Pick your answer: \n ')
-get_user_answer_4(answer)  
 
-def get_user_answer_5(user_answer):
-    if answer.lower() == 'c':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
+question_1 = "1. What does BMW stand for? \n A. Bayerische Motoren Werke \n B. Brunswick Motoren Werke \n C. Borgholzzhausen Motoren Werke\n D. Berlin Motorenn Werke"
+question_2 = "2. What color is the BMW logo? \n A. Blue & gold \n B. Red & gold \n C. Red & White \n D. Blue & white"
+question_3 = "3. What is the slogan of BMW? \n A. The Ultimate Driving Power \n B. Sheer Driving Pleasure\n C. Enjoy the Ride \n D. Joy of discovery"
+question_4 = "4. What is the BMW M series? \n A. A line of electric cars \n B. An SUV for off-roading \n C. A line of performance cars \n D. A line of luxury sedan"
+question_5 = "5. What was the name of BMW\'s first car? \n A. 3 Series \n B. 5 Series \n C. 7 Series\n D. Dixi"
+question_6 = "6. What year was the BMW 3 Series first intoduced? \n A. 1969 \n B. 1961 \n C. 1975\n D. 1984"
+question_7 = "7. How far could BMW\'s first all-electric car drive before running out of battery? \n A. 79 miles\n B. 59 miles \n C. 19 miles\n D. 39 miles"
+question_8 = "8. What is BMW\'s Formula One racing team called? \n A. BMW Motorsport \n B. BMW Sauber F1 Team \n C. BMW M Racing\n D. BMW Racing Team"
+question_9 = "9. What is BMW\'s headquartwes shaped like? \n A. A Radiator \n B. A catalytic converter \n C. A Piston\n D. A Four-Cylinder Engine"
+question_10 = "10. What was BMW\'s first product? \n A. Aircraft Engine \n B. Spark plug \n C. A Tractor\n D. Motorcycle"
+question_11 = "11. What grille design has become a defining feature of BMW\'s automobiles? \n A. Heart grille \n B. Kidney grille \n C. Pharynx grille\n D. Liver grille"
+question_12 = "12. What race did the BMW 328 win in 1936? \n A. Alpine Rally \n B. Österreichise Alpenfahrt \n C. Mille Miglia\n D. Eifelrennen"
+question_13 = "13. Which BMW features vertically sliding doors? \n A. M1 \n B. E12"
+question_14 = "14. What is the largest BMW ever built? \n A. X7\n B. M12 \n C. Z4\n D. E90"
+question_15 = "15. Which BMW was known as a bubble car? \n A. HEINKEL KABINE\n B. ATTICA \n C. PULI\n D. ISETTA"
+question_16 = "16. What year was BMW founded? \n A. 1926 \n B. 1896 \n C. 1906\n D. 1916"
+question_17 = "17. What was BMW\'s first motorcycle? \n A. K100 \n B. K1 \n C. R32\n D. R12"
+question_18 = "18. The 303 was the first BMW with a ______. \n A. V6 Engine\n B. V8 Engine\n C. V4 Engine\n D. V12 Engine"
+question_19 = "19. What was the company\'s first post WW11 vehicle? \n A. BMW Z1 \n B. BMW 326 \n C. BMW 501\n D. BMW 700"
+question_20 = "20. Which BMW was almost a Lamborghini? \n A. E12\n B. M1\n C. Z1\n D. I8"
+questions = {question_1: 'a', question_2: 'd',question_3: 'b', question_4: 'c', question_5: 'd', question_6: 'c', question_7: 'c', question_8: 'b', question_9: 'd', 
+             question_10:'a', question_11: 'b', question_12: 'd', question_13:'d', question_14: 'a', question_15: 'd', question_16: 'd', question_17: 'c', 
+             question_18: 'a', question_19: 'c', question_20:'b'}
+for i in questions:
+    print(i)
+    answer = input("pick you answer: \n")
+    if answer.lower() == questions[i]:
+        print(f"[bold blue]Welldone! {answer} is the correct answer :smile:")
         score += 1
     else:
-        print('[blue]"Sorry...option c is the correct answer"')
-answer = input('[bold blue]6. What year was the BMW 3 Series first intoduced? \n A. 1969 \n B. 1961 \n C. 1975\n D. 1984 \n Pick your answer: \n')
-get_user_answer_5(answer)  
-
-def get_user_answer_6(user_answer):
-    if answer.lower() == 'c':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option c is the correct answer"')
-answer = input('[bold blue]7. How far could BMW\'s first all-electric car drive before running out of battery? \n A. 79 miles\n B. 59 miles \n C. 19 miles\n D. 39 miles\n Pick your answer: \n')
-get_user_answer_6(answer) 
-
-def get_user_answer_7(user_answer):
-    if answer.lower() == 'b':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option b is the correct answer"')
-answer = input('[bold white]8. What is BMW\'s Formula One racing team called? \n A. BMW Motorsport \n B. BMW Sauber F1 Team \n C. BMW M Racing\n D. BMW Racing Team \n Pick your answer: \n')
-get_user_answer_7(answer)
-
-def get_user_answer_8(user_answer):
-    if answer.lower() == 'd':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option d is the correct answer"')
-answer = input('[bold blue]9. What is BMW\'s headquartwes shaped like? \n A. A Radiator \n B. A catalytic converter \n C. A Piston\n D. A Four-Cylinder Engine \n Pick your answer: \n')
-get_user_answer_8(answer)
-
-def get_user_answer_9(user_answer):
-    if answer.lower() == 'a':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option a is the correct answer"')
-answer = input('[bold blue]10. What was BMW\'s first product? \n A. Aircraft Engine \n B. Spark plug \n C. A Tractor\n D. Motorcycle \n Pick your answer: \n')
-get_user_answer_9(answer)
-
-def get_user_answer_10(user_answer):
-    if answer.lower() == 'b':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option b is the correct answer"')
-answer = input('[bold blue]11. What grille design has become a defining feature of BMW\'s automobiles? \n A. Heart grille \n B. Kidney grille \n C. Pharynx grille\n D. Liver grille \n Pick your answer: \n')
-get_user_answer_10(answer)
-
-def get_user_answer_11(user_answer):
-    if answer.lower() == 'd':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option d is the correct answer"')
-answer = input('[bold blue]12. What race did the BMW 328 win in 1936? \n A. Alpine Rally \n B. Österreichise Alpenfahrt \n C. Mille Miglia\n D. Eifelrennen \n Pick your answer: \n')
-get_user_answer_11(answer)
-
-def get_user_answer_12(user_answer):
-    if answer.lower() == 'd':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option d is the correct answer"')
-answer = input('[bold blue]13. Which BMW features vertically sliding doors? \n A. M1 \n B. E12 \n C. I8\n D. Z1 \n Pick your answer: \n')
-get_user_answer_12(answer)
-
-def get_user_answer_13(user_answer):
-    if answer.lower() == 'a':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option a is the correct answer"')
-answer = input('[bold blue]14. What is the largest BMW ever built? \n A. X7\n B. M12 \n C. Z4\n D. E90 \n Pick your answer: \n')
-get_user_answer_13(answer)
-
-def get_user_answer_14(user_answer):
-    if answer.lower() == 'd':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option d is the correct answer"')
-answer = input('[bold blue]15. Which BMW was known as a "bubble car"? \n A. HEINKEL KABINE\n B. ATTICA \n C. PULI\n D. ISETTA \n Pick your answer: \n')
-get_user_answer_14(answer)
-
-def get_user_answer_15(user_answer):
-    if answer.lower() == 'd':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option d is the correct answer"')
-answer = input('[bold blue]16. What year was BMW founded? \n A. 1926 \n B. 1896 \n C. 1906\n D. 1916 \n Pick your answer: \n')
-get_user_answer_15(answer)
-
-def get_user_answer_16(user_answer):
-    if answer.lower() == 'c':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option c is the correct answer"')
-answer = input('[bold blue]17. What was BMW\'s first motorcycle? \n A. K100 \n B. K1 \n C. R32\n D. R12 \n Pick your answer: \n')
-get_user_answer_16(answer)
-
-def get_user_answer_17(user_answer):
-    if answer.lower() == 'a':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option a is the correct answer"')
-answer = input('[bold blue]18. The 303 was the first BMW with a ______. \n A. V6 Engine\n B. V8 Engine\n C. V4 Engine\n D. V12 Engine \n Pick your answer: \n')
-get_user_answer_17(answer)
-
-def get_user_answer_18(user_answer):
-    if answer.lower() == 'c':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option c is the correct answer"')
-answer = input('[bold blue]19. What was the company\'s first post WW11 vehicle? \n A. BMW Z1 \n B. BMW 326 \n C. BMW 501\n D. BMW 700 \n Pick your answer: \n')
-get_user_answer_18(answer)
-
-def get_user_answer_19(user_answer):
-    if answer.lower() == 'b':
-        print(f"[bold blue]Welldone! {user_answer} is the correct answer :smile:")
-        global score
-        score += 1
-    else:
-        print('[blue]"Sorry...option b is the correct answer"')
-answer = input('[bold blue]20. Which BMW was almost a Lamborghini? \n A. E12\n B. M1\n C. Z1\n D. I8 \n Pick your answer: \n')
-get_user_answer_19(answer)
+        print('none')
+            
+            
 
 
 def get_result(user_score):
